fd866528312bbe0987dcdf5906b0ec60f1484d80
Lâm Xuân Nguyên <100450698+imnguin@users.noreply.github.com>
2025-05-15 22:14:09 +0700

    Update SystemVars.js

--- src/utils/constants/SystemVars.js
+++ src/utils/constants/SystemVars.js
@@ -2,6 +2,6 @@ export const HOSTNAME = 'NODESERVER'
 export const HOST_LIST = {
     [HOSTNAME] : {
         hostName : [HOSTNAME],
-        hostBaseURL : 'http://localhost:8080/'
+        hostBaseURL : 'https://viettustore-server.onrender.com/'
     }
 }
